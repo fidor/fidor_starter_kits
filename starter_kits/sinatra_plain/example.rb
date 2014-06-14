64e39bc61e28d2b68eb3d8b7c4f0741d7ffbb531
Georg Leciejewski <gl@salesking.eu>
2014-06-14 19:43:51 +0200

    comment sinatra plain

--- starter_kits/sinatra_plain/example.rb
+++ starter_kits/sinatra_plain/example.rb
@@ -4,27 +4,20 @@ require 'active_support/json'
 require 'net/http'
 
 get '/' do
-  # # settings
-  # @url = 'http://localhost:4567'
-  # @id = '4d25677ffd39b748'
-  # @secret = 'f263d0470c0e073b6894d3d6e794aaa2'
-  # @fidor_url = 'http://localhost:3000'
-
+  # settings
   @url = 'http://localhost:4567'
   @id = '<CLIENT-ID>'
   @secret = '<CLIENT-SECRET>'
   @fidor_url = '<FIDOR-URL>'
 
-
-  # redirect to authorize url
+  # 1. redirect to authorize url
   unless code = params["code"]
     dialog_url = "#{@fidor_url}/oauth/authorize?client_id=#{@id}&redirect_uri=#{CGI::escape(@url)}"
     redirect dialog_url
   end
 
-  # get the access token
+  # 2. get the access token, with code returned from auth dialog above
   token_url = URI("#{@fidor_url}/oauth/token")
-  # token_url = "#{@fidor_url}/oauth/token?client_id=#{@id}&redirect_uri=#{CGI::escape(@url)}&client_secret=#{@secret}&code=#{code}"
   # GET and parse access_token response json
   res = Net::HTTP.post_form(token_url, 'client_id' => @id,
                                         'redirect_uri' => CGI::escape(@url),
@@ -33,7 +26,6 @@ get '/' do
   resp = ActiveSupport::JSON.decode(res.body)
 
   "Now you have an access token: #{resp.inspect}"
-
 
   # GET info about current user
   # usr_url = "#{@fidor_url}/api/users/current?access_token=#{resp['access_token']}"
