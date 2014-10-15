64fd968e711ab556895dd41b5100252927ad55c3
Tim Becker <tim.becker@kuriositaet.de>
2014-10-15 14:25:41 +0200

    preps for release

---
+++ starter_kits/golang_plain/README.md
@@ -0,0 +1,19 @@
+# Basic Go Example
+
+This is a basic example project that demostrates the Fidor API OAuth
+flow. It provides the same functionality as the php example if you would
+like to compare the two sample projects.
+
+## Usage
+
+In order to run the example you'll need a Go environment installed (see
+www.golang.org) and can start the sample with:
+
+    go run example.go
+
+The server will listen on port 8080, you can access the example in your
+browser under the following URL:
+
+  http://localhost:8080
+
+
--- starter_kits/golang_plain/example.go
+++ starter_kits/golang_plain/example.go
@@ -11,42 +11,45 @@ package main
 //
 //     $ http://localhost:8080
 
-
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
-	"encoding/json"
 )
-
 
 // The following sections defines the settings you require for the
 // app to be able to connect to and authorize itself against the api.
 
 // app ID and secret, can be found in this apps "Details" page in the
 // AppManager.
-var client_id          = "0294a68c2dd7925e"
-var client_secret      = "57c27addb97002f24bd75e604e221e04"
+var client_id = "<CLIENT_ID>"
+var client_secret = "<CLIENT_SECRET>"
+
 // Fidor's OAuth Endpoint (this changes between Sandbox and Production)
-var fidor_oauth_url = "http://localhost:4001/oauth"  // e.g https://fidor.com/api_sandbox/oauth
+var fidor_oauth_url = "<FIDOR_OAUTH_URL>" // e.g https://fidor.com/api_sandbox/oauth
 // The OAuth Endpoint this App provides
-var oauth_cb_url    = "http://localhost:8080"
+var oauth_cb_url = "<APP_URL>"
+
 // The URL of the Fidor API (this changes between Sandbox and
 // Production)
-var fidor_api_url   = "http://localhost:4001"   // e.g https://fidor.com/api_sandbox vs /api
+var fidor_api_url = "<FIDOR_API_URL>" // e.g https://fidor.com/api_sandbox vs /api
+
+
+
 
 func main() {
-		// register a handler function (see next function) to service
-		// requests ...
-    http.HandleFunc("/", indexHandler)
-		// ... and start listening.
-    http.ListenAndServe(":8080", nil)
+	// register a handler function (see next function) to service
+	// requests ...
+	http.HandleFunc("/", indexHandler)
+	// ... and start listening.
+	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	// ignore any favicon requests, etc.
-	if (r.URL.Path != "/") {
+	if r.URL.Path != "/" {
 		w.WriteHeader(404)
 		return
 	}
@@ -57,8 +60,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if values["code"] != nil {
 		// retrieve the actual OAuth access token ....
 		code := values.Get("code")
-		if token,err := retrieveTokenFromCode(code) ; err != nil {
-			fmt.Printf("err: %v\n",err)
+		if token, err := retrieveTokenFromCode(code); err != nil {
+			fmt.Printf("err: %v\n", err)
 			w.WriteHeader(500)
 			fmt.Fprintf(w, "Unfortunately, an error occured retrieving oauth token")
 		} else {
@@ -66,18 +69,19 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 			renderWelcome(w, token)
 		}
 	} else {
-	// we don't have an oauth `code` yet, so we need to 
-	// redirect the user to the OAuth provider to get one ...
-		oauth_url := fmt.Sprintf( "%s/authorize?client_id=%s&redirect_uri=%s",
-															fidor_oauth_url,
-															client_id,
-															url.QueryEscape(oauth_cb_url))
+		// we don't have an oauth `code` yet, so we need to
+		// redirect the user to the OAuth provider to get one ...
+		oauth_url := fmt.Sprintf("%s/authorize?client_id=%s&redirect_uri=%s",
+			fidor_oauth_url,
+			client_id,
+			url.QueryEscape(oauth_cb_url))
 
-		header    := w.Header()
+		header := w.Header()
 		header.Add("location", oauth_url)
 		w.WriteHeader(307)
 	}
 }
+
 
 // Our TokenResponse representation used to pick it out from the JSON
 // returned by the OAuth server.
@@ -87,16 +91,16 @@ type TokenResponse struct {
 
 // Use the OAuth code that the user's browser picked up from the OAuth
 // server to request an OAuth access_token to use in API requests.
-func retrieveTokenFromCode(code string)(token string, err error) {
+func retrieveTokenFromCode(code string) (token string, err error) {
 	// assemble the API endpoint URL and request payload
-	tokenUrl     := fmt.Sprintf("%s/token", fidor_oauth_url)
+	tokenUrl := fmt.Sprintf("%s/token", fidor_oauth_url)
 	tokenPayload := url.Values{
-		"client_id"     : {client_id},
-		"client_secret" : {client_secret},
-		"code"          : {code},
+		"client_id":     {client_id},
+		"client_secret": {client_secret},
+		"code":          {code},
 	}
 	// Call API
-	if resp, err := http.PostForm(tokenUrl,tokenPayload); err != nil {
+	if resp, err := http.PostForm(tokenUrl, tokenPayload); err != nil {
 		return "", err
 	} else {
 		// if successful, pick the access_token out of the reply.
@@ -110,6 +114,7 @@ func retrieveTokenFromCode(code string)(token string, err error) {
 	}
 }
 
+
 // Our server code only makes a single call to the API to retrieve user
 // information. This is our internal representation of the returned JSON
 // used to pick out the user's email.
@@ -118,20 +123,21 @@ type UserResponse struct {
 }
 
 // function to retreive user information from the API
-func getUser(token string)(u UserResponse, err error) {
+func getUser(token string) (u UserResponse, err error) {
 	// Assemble endpoint URL...
 	url := fmt.Sprintf("%s/users/current?access_token=%s", fidor_api_url, token)
 	if resp, err := http.Get(url); err != nil {
-		return u,err
+		return u, err
 	} else {
 		decoder := json.NewDecoder(resp.Body)
 		if err = decoder.Decode(&u); err != nil {
-			return u,err
+			return u, err
 		} else {
-			return u,nil
+			return u, nil
 		}
 	}
 }
+
 
 // once all the OAuth calls have been taken care of, this function is
 // called from the http handler. It retrieves the user's email address
@@ -139,16 +145,14 @@ func getUser(token string)(u UserResponse, err error) {
 
 func renderWelcome(w http.ResponseWriter, token string) {
 	if user, err := getUser(token); err != nil {
-		fmt.Printf("err: %v\n",err)
+		fmt.Printf("err: %v\n", err)
 		w.WriteHeader(500)
 	} else {
-		txLink    := fmt.Sprintf("%s/transactions?access_token=%s", fidor_api_url,token)
-		acctsLink := fmt.Sprintf("%s/accounts?access_token=%s", fidor_api_url,token)
-		fmt.Fprintf(w,indexTemplate, user.Email, token, txLink, acctsLink)
+		txLink := fmt.Sprintf("%s/transactions?access_token=%s", fidor_api_url, token)
+		acctsLink := fmt.Sprintf("%s/accounts?access_token=%s", fidor_api_url, token)
+		fmt.Fprintf(w, indexTemplate, user.Email, token, txLink, acctsLink)
 	}
 }
-
-
 
 var indexTemplate = `
 <html>
